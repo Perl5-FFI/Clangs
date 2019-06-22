44dde09c6a5d3ab53a06fbeeacf2492953194b84
Graham Ollis <plicease@cpan.org>
2019-06-22 07:50:33 -0400

    Initial commit

---
+++ lib/FFI/Clang.pm
@@ -0,0 +1,10 @@
+package FFI::Clang;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Interface to libclang useful for FFI development
+# VERSION
+
+1;
---
+++ t/ffi_clang.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use FFI::Clang;
+
+ok 1, 'todo';
+
+done_testing
